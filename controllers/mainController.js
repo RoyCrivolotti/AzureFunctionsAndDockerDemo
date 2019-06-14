a66904591bbad2b84d817ab439e1b8946468e8ae
Roy Crivolotti <roycrivolotti@gmail.com>
2019-06-14 14:57:33 -0300

    Added dotenv to use IP env var instead of localhost:port

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,31 +1,33 @@
 const axios = require('../node_modules/axios');
+const dotenv = require('../node_modules/dotenv');
 
 function getProduct(req, res) {
-	axios.get(`http://localhost:7071/api/products/get/${req.query.ProductId}`)
+	axios.get(`http://${process.env.IP}:7071/api/products/get/${req.query.ProductId}`)
 		.then(response => res.render('/product', { product: response.data }))
 		.catch(err => res.render('error', { message: 'Something went wrong :/', err }));
 }
+
 function getProducts(req, res) {
-	axios.get('http://localhost:7071/api/products/getall')
+	axios.get(`http://${process.env.IP}:7071/api/products/getall`)
 		.then(response => res.render('index', { title: 'This is a super basic app to consume an Azure Functions API demo :)',
 			products: response.data }))
 		.catch(err => res.render('error', { message: 'Something went wrong :/', err }));
 }
 
 function createProduct(req, res) {
-	axios.post('http://localhost:7071/api/products/create/', { productName: req.body.ProductName })
+	axios.post(`http://${process.env.IP}:7071/api/products/create/`, { productName: req.body.ProductName })
 		.then(response => res.render('index', { products: response.data }))
 		.catch(err => res.render('error', { message: 'Something went wrong :/', err }));
 }
 
 function deleteProduct(req, res) {
-	axios.delete(`http://localhost:7071/api/products/delete/${req.body.ProductId}`)
+	axios.delete(`http://${process.env.IP}:7071/api/products/delete/${req.body.ProductId}`)
 		.then(response => res.render('index', { products: response.data }))
 		.catch(err => res.render('error', { message: 'Something went wrong :/', err }));
 }
 
 function updateProduct(req, res) {
-	axios.put(`http://localhost:7071/api/products/update/${req.body.ProductId}`, { productName: req.body.ProductName })
+	axios.put(`http://${process.env.IP}:7071/api/products/update/${req.body.ProductId}`, { productName: req.body.ProductName })
 		.then(response => res.render('index', { products: response.data }))
 		.catch(err => res.render('error', { message: 'Something went wrong :/', err }));
 }
