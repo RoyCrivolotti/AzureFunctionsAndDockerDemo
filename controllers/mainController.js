eac580f5a21da851242cedc5ed457ef29b691218
Roy Crivolotti <roycrivolotti@gmail.com>
2019-11-11 17:20:28 -0300

    Controller uses name over IP, which is dynamic

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -4,33 +4,32 @@ const axios = require('../node_modules/axios');
 require('../node_modules/dotenv').config();
 
 function getProduct(req, res) {
-	axios.get(`http://${process.env.IP}/api/products/get/${req.query.ProductId}`)
+	axios.get(`http://serverlessapp/api/products/get/${req.query.ProductId}`)
 		.then(response => res.render('/product', { product: response.data }))
 		.catch(err => res.render('error', { message: 'Something went wrong :/', err }));
 }
 
 function getProducts(req, res) {
-	console.log(`IP: ${process.env.IP}`);
-	axios.get(`http://${process.env.IP}/api/products/getall`)
+	axios.get('http://serverlessapp/api/products/getall')
 		.then(response => res.render('index', { title: 'This is a super basic app to consume an Azure Functions API demo :)',
 			products: response.data }))
 		.catch(err => res.render('error', { message: 'Something went wrong :/', err }));
 }
 
 function createProduct(req, res) {
-	axios.post(`http://${process.env.IP}/api/products/create/`, { productName: req.body.ProductName })
+	axios.post('http://serverlessapp/api/products/create/', { productName: req.body.ProductName })
 		.then(response => res.render('index', { products: response.data }))
 		.catch(err => res.render('error', { message: 'Something went wrong :/', err }));
 }
 
 function deleteProduct(req, res) {
-	axios.delete(`http://${process.env.IP}/api/products/delete/${req.body.ProductId}`)
+	axios.delete(`http://serverlessapp/api/products/delete/${req.body.ProductId}`)
 		.then(response => res.render('index', { products: response.data }))
 		.catch(err => res.render('error', { message: 'Something went wrong :/', err }));
 }
 
 function updateProduct(req, res) {
-	axios.put(`http://${process.env.IP}/api/products/update/${req.body.ProductId}`, { productName: req.body.ProductName })
+	axios.put(`http://serverlessapp/api/products/update/${req.body.ProductId}`, { productName: req.body.ProductName })
 		.then(response => res.render('index', { products: response.data }))
 		.catch(err => res.render('error', { message: 'Something went wrong :/', err }));
 }
