aa3e24e630aef43bee68a6257cc02f5184d1edb3
RoyCrivolotti <roy.crivolotti@southworks.com>
2019-12-10 17:14:22 -0300

    Commented out unhandled route

--- NodeApp/routes/index.js
+++ NodeApp/routes/index.js
@@ -4,7 +4,7 @@ const controller = require('../controllers/mainController');
 const router = express.Router();
 
 router.get('/', controller.getProducts);
-router.get('/getProduct', controller.getProduct);
+// router.get('/getProduct', controller.getProduct);
 router.post('/createProduct', controller.createProduct);
 router.post('/updateProduct', controller.updateProduct);
 router.post('/deleteProduct', controller.deleteProduct);
